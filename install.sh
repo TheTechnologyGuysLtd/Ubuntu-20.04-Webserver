8f0cfe0ad87467e999e5c32bf501f5a7e91a39df
TheTechnologyGuysLtd <73659991+TheTechnologyGuysLtd@users.noreply.github.com>
2020-10-29 22:51:02 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -16,6 +16,13 @@ echo "##########################################################################
 "
 sleep 5
 
+read -p "Please Enter your website domain exsample.com:" ServerName
+read -p "Please Enter your website domain alias www.exsample.com:" ServerAlias
+read -p "Please Enter Database Name:" dbname
+read -p "Please enter the username you wish to create : " username
+read -p "Please Enter Host To Allow Access Eg: %,ip or hostname : " host
+read -p "Please Enter the Password for the user ($username) : " password
+
 # update ubuntu 20.04
 apt update
 apt -y upgrade
@@ -35,9 +42,6 @@ apt -y install php7.4-common php7.4-mysql php7.4-curl php7.4-json php7.4-zip php
 # Setup the website config file
 cp /etc/apache2/sites-available/000-default.conf /etc/apache2/sites-available/000-default.conf.old
 rm /etc/apache2/sites-available/000-default.conf
-
-read -p "Please Enter your website domain exsample.com:" ServerName
-read -p "Please Enter your website domain alias www.exsample.com:" ServerAlias
 
 echo "<VirtualHost *:80>
     ServerAdmin webmaster@thetechguys.site
@@ -65,12 +69,6 @@ a2enmod ssl
 ######## START OF MYSQL  INSTALL##################
 apt -y update
 apt -y install mysql-server
-
-read -p "Please Enter Database Name:" dbname
-read -p "Please enter the username you wish to create : " username
-read -p "Please Enter Host To Allow Access Eg: %,ip or hostname : " host
-read -p "Please Enter the Password for the user ($username) : " password
-
 mysql -e "CREATE USER '$username'@'$host' IDENTIFIED BY '$password'";
 mysql -e "CREATE DATABASE $dbname";
 mysql -e "GRANT ALL PRIVILEGES ON $dbname.* TO '$username'@'$host'";
@@ -86,17 +84,6 @@ sudo systemctl restart mysql
 
 # restart apache2
 systemctl restart apache2
-
-# remove default index file and copying website ####
-# rm /var/www/html/index.html
-# sudo apt -y install zip unzip
-# read -p "your website download link example.com/files.zip:"  download
-# wget $download
-# read -p "zip name example.zip:"  zip
-# unzip $zip -d /var/www/html
-# chown -R www-data:www-data storage
-# chown -R www-data:www-data bootstrap
-
 
 ######## SETUP FIREWALL ###############
 sudo apt -y install ufw
@@ -115,6 +102,17 @@ apt -y install certbot python3-certbot-apache
 systemctl reload apache2
 certbot --apache --agree-tos --redirect
 ######## END OF SSL INSTALL##################
+
+##### emove default index file and copying website ######
+# rm /var/www/html/index.html
+# sudo apt -y install zip unzip
+# read -p "your website download link example.com/example.zip:"  download
+# wget $download
+# read -p "zip name example.zip:"  zip
+# unzip $zip -d /var/www/html
+# chown -R www-data:www-data storage
+# chown -R www-data:www-data bootstrap
+##### emove default index file and copying website ######
 
 # Final message
 
@@ -138,7 +136,7 @@ echo "##########################################################################
       #                     echo "Your username is "     $ServerAlias                             #
       #                     echo "Your password is "     $ServerAlias                             #  
       #                     echo "Your host is "         $ServerAlias                             #  
-      #                                                                                           #  
+      #       YOU MUST MAKE A NOT OF THIS INFORMATION JUST NOW ITS NOT SAVED ON EXIT              #  
       #                                                                                           #
       #############################################################################################
 "
