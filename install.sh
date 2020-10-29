c52d5f3eecf4ad76c9eb566f240594d8ae08056d
TheTechnologyGuysLtd <73659991+TheTechnologyGuysLtd@users.noreply.github.com>
2020-10-29 22:37:54 +0000

    Update install.sh
    now all in one file

--- install.sh
+++ install.sh
@@ -1,29 +1,58 @@
 #!/bin/sh
 ######## START OF WEBSERVER INSTALL##################
-echo We are now going to install a Webserver on Ubutnu 20.04 With
-echo Apache2, php7.4, MySQL, PhpMyAdmin, Lets Encrypt
-echo Waiting 10 Seconds Press Ctr + c to cancel
-sleep 10
+echo "#########################################################################################################
+      #               This bash file will install a full webserver on ubntu 20.04                 #
+      #                             Created By The Technology Guys Ltd                            #
+      #                                                                                           #
+      #                                          Install                                          #
+      #                   Apache2, php7.4, MySQL, PhpMyAdmin, Lets Encrypt                        #  
+      #                                                                                           #     
+      #                                       PHP Extensions                                      #
+      #  php7.4-common php7.4-mysql php7.4-curl php7.4-json php7.4-zip php7.4-cgi php7.4-opcache  #
+      #              php7.4-mbstring php7.4-xml php7.4-bcmath php7.4-mysqli php7.4-gd             #
+      #                                                                                           #
+      #                                                                                           #
+      #############################################################################################
+"
+sleep 5
 
 # update ubuntu 20.04
 apt update
 apt -y upgrade
 
 # Add additional repositories
-add-apt-repository ppa:ondrej/php
-add-apt-repository ppa:ondrej/apache2
+echo "ENTER" | add-apt-repository ppa:ondrej/php
+echo "ENTER" | add-apt-repository ppa:ondrej/apache2
 apt -y update
 
 # Install software
 apt -y install software-properties-common
 apt -y install apache2
 apt -y install php7.4
-apt install php7.4-common php7.4-mysql php7.4-curl php7.4-json php7.4-zip php7.4-cgi php7.4-opcache php7.4-mbstring php7.4-xml php7.4-bcmath php7.4-mysqli php7.4-gd  
-apt -y install net-tools
+apt -y install php7.4-common php7.4-mysql php7.4-curl php7.4-json php7.4-zip php7.4-cgi php7.4-opcache php7.4-mbstring php7.4-xml php7.4-bcmath php7.4-mysqli php7.4-gd
+#apt -y install net-tools
 
 # Setup the website config file
+cp /etc/apache2/sites-available/000-default.conf /etc/apache2/sites-available/000-default.conf.old
 rm /etc/apache2/sites-available/000-default.conf
-cp 000-default.conf /etc/apache2/sites-available/
+
+read -p "Please Enter your website domain exsample.com:" ServerName
+read -p "Please Enter your website domain alias www.exsample.com:" ServerAlias
+
+echo "<VirtualHost *:80>
+    ServerAdmin webmaster@thetechguys.site
+    ServerName $ServerName
+    ServerAlias $ServerAlias
+    DocumentRoot /var/www/html/public
+    ErrorLog ${APACHE_LOG_DIR}/error.log
+    CustomLog ${APACHE_LOG_DIR}/access.log combined
+<Directory /var/www/html/public>
+    Options Indexes FollowSymLinks MultiViews
+    AllowOverride All
+    Order allow,deny
+    allow from all
+ </Directory>
+</VirtualHost>" > /etc/apache2/sites-available/000-default.conf
 
 # Activate the sites config
 a2ensite 000-default.conf
@@ -33,6 +62,50 @@ a2enmod rewrite
 a2enmod ssl
 ######## END  OF WEBSERVER INSTALL##################
 
+######## START OF MYSQL  INSTALL##################
+apt -y update
+apt -y install mysql-server
+
+read -p "Please Enter Database Name:" dbname
+read -p "Please enter the username you wish to create : " username
+read -p "Please Enter Host To Allow Access Eg: %,ip or hostname : " host
+read -p "Please Enter the Password for the user ($username) : " password
+
+mysql -e "CREATE USER '$username'@'$host' IDENTIFIED BY '$password'";
+mysql -e "CREATE DATABASE $dbname";
+mysql -e "GRANT ALL PRIVILEGES ON $dbname.* TO '$username'@'$host'";
+
+### SECURE MYSQL INSTALL ###
+# mysql_secure_installation
+######## END OF MYSQL INSTALL##################
+
+######## START OF PHPMYADMIN INSTALL##################
+sudo apt -y install phpmyadmin
+sudo systemctl restart mysql
+######## END OF PHPMYADMIN INSTALL##################
+
+# restart apache2
+systemctl restart apache2
+
+# remove default index file and copying website ####
+# rm /var/www/html/index.html
+# sudo apt -y install zip unzip
+# read -p "your website download link example.com/files.zip:"  download
+# wget $download
+# read -p "zip name example.zip:"  zip
+# unzip $zip -d /var/www/html
+# chown -R www-data:www-data storage
+# chown -R www-data:www-data bootstrap
+
+
+######## SETUP FIREWALL ###############
+sudo apt -y install ufw
+ufw allow http
+ufw allow https
+ufw allow ssh
+ufw allow mysql
+echo "y" | sudo ufw enable
+######## SETUP FIREWALL ###############
 
 ######## START OF SSL INSTALL##################
 # install ssl on the server
@@ -40,57 +113,35 @@ apt -y update
 apt -y upgrade
 apt -y install certbot python3-certbot-apache
 systemctl reload apache2
-certbot --apache
+certbot --apache --agree-tos --redirect
 ######## END OF SSL INSTALL##################
 
-######## START OF MYSQL  INSTALL##################
-apt -y update
-apt -y install mysql-server
-
-### SECURE MYSQL INSTALL ###
-mysql_secure_installation
-######## END OF MYSQL INSTALL##################
-
-######## START OF PHPMYADMIN INSTALL##################
-sudo -y apt install phpmyadmin
-sudo a2enconf phpmyadmin.conf
-######## END OF PHPMYADMIN INSTALL##################
-
-
-
-# restart apache2
-systemctl restart apache2
-
-# remove default index file
-rm /var/www/html/index.html
-cp index.html /var/www/html/
-
 # Final message
-echo Your webserver is now installed with Apache and php7.4
 
-# Output server info here
-cat /etc/apache2/sites-available/000-default.conf
+echo "#########################################################################################################
+      #                               Your webserver is now installed                             #
+      #                                                                                           #
+      #                                                                                           #
+      #                                          Install                                          #
+      #                   Apache2, php7.4, MySQL, PhpMyAdmin, Lets Encrypt                        #  
+      #                                                                                           #     
+      #                                       PHP Extensions                                      #
+      #  php7.4-common php7.4-mysql php7.4-curl php7.4-json php7.4-zip php7.4-cgi php7.4-opcache  #
+      #              php7.4-mbstring php7.4-xml php7.4-bcmath php7.4-mysqli php7.4-gd             #
+      #                                                                                           #                               
+      #                                     Domain Info:                                          #
+      #                     echo "Your server domain is " $ServerName                             #  
+      #                     echo "Your server alias is "  $ServerAlias                            #
+      #                                                                                           #  
+      #                                     MySQL Info:                                           #     
+      #                     echo "Your database name is " $ServerName                             #      
+      #                     echo "Your username is "     $ServerAlias                             #
+      #                     echo "Your password is "     $ServerAlias                             #  
+      #                     echo "Your host is "         $ServerAlias                             #  
+      #                                                                                           #  
+      #                                                                                           #
+      #############################################################################################
+"
+                       
 # Exit the script
 exit 0
-
-
-
-#!/bin/bash
-# Description: Set up MySQL Community Release 5.7
-
-# Get the repo RPM and install it.
-wget http://dev.mysql.com/get/mysql57-community-release-el7-7.noarch.rpm 
-yum -y install ./mysql57-community-release-el7-7.noarch.rpm 
-
-# Install the server and start it
-yum -y install mysql-community-server 
-systemctl start mysqld 
-
-# Get the temporary password
-temp_password=$(grep password /var/log/mysqld.log | awk '{print $NF}')
-
-# Set up a batch file with the SQL commands
-echo "CREATE USER 'root'@'localhost' IDENTIFIED BY 'Newhakase-labs123@'; flush privileges;" > reset_pass.sql
-
-# Log in to the server with the temporary password, and pass the SQL file to it.
-mysql -u root --password="$temp_password" --connect-expired-password < reset_pass.sql
