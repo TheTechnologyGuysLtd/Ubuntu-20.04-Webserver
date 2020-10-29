bcc982cdbeba7bc53181d02de5b9245df815ef1d
TheTechnologyGuysLtd <73659991+TheTechnologyGuysLtd@users.noreply.github.com>
2020-10-29 23:51:49 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -18,10 +18,25 @@ sleep 5
 
 read -p "Please Enter your website domain exsample.com:" ServerName
 read -p "Please Enter your website domain alias www.exsample.com:" ServerAlias
+Read -p "Please Enter your ServerAdmin email webmaster@exsample.com:"  ServerAdmin
 read -p "Please Enter Database Name:" dbname
 read -p "Please enter the username you wish to create : " username
 read -p "Please Enter Host To Allow Access Eg: %,ip or hostname : " host
 read -p "Please Enter the Password for the user ($username) : " password
+
+
+echo "
+    Domain names:
+    ServerName $ServerName
+    ServerAlias $ServerAlias
+    ServerAdmin $ServerAdmin
+    
+    MySQL info:
+    Database name: $dbname
+    Username: $username
+    Password: $password
+    host: $host" > /var/www/info.txt
+
 
 # update ubuntu 20.04
 apt update
@@ -44,7 +59,7 @@ cp /etc/apache2/sites-available/000-default.conf /etc/apache2/sites-available/00
 rm /etc/apache2/sites-available/000-default.conf
 
 echo "<VirtualHost *:80>
-    ServerAdmin webmaster@thetechguys.site
+    ServerAdmin $ServerAdmin
     ServerName $ServerName
     ServerAlias $ServerAlias
     DocumentRoot /var/www/html/public
@@ -79,11 +94,7 @@ mysql -e "GRANT ALL PRIVILEGES ON $dbname.* TO '$username'@'$host'";
 
 ######## START OF PHPMYADMIN INSTALL##################
 sudo apt -y install phpmyadmin
-sudo systemctl restart mysql
 ######## END OF PHPMYADMIN INSTALL##################
-
-# restart apache2
-systemctl restart apache2
 
 ######## SETUP FIREWALL ###############
 sudo apt -y install ufw
@@ -99,23 +110,25 @@ echo "y" | sudo ufw enable
 apt -y update
 apt -y upgrade
 apt -y install certbot python3-certbot-apache
-systemctl reload apache2
 certbot --apache --agree-tos --redirect
 ######## END OF SSL INSTALL##################
 
+##### Remove default index file and copying website ######
+# rm /var/www/html/index.html
+# sudo apt -y install zip unzip
+# read -p "your website download link example.com/example.zip:"  download
+# wget $download
+# read -p "zip name example.zip:"  zip
+# unzip $zip -d /var/www/html
+# chown -R www-data:www-data storage
+# chown -R www-data:www-data bootstrap
 ##### emove default index file and copying website ######
-rm /var/www/html/index.html
-sudo apt -y install zip unzip
-read -p "your website download link example.com/example.zip:"  download
-wget $download
-read -p "zip name example.zip:"  zip
-unzip $zip -d /var/www/html
-chown -R www-data:www-data storage
-chown -R www-data:www-data bootstrap
-##### emove default index file and copying website ######
+
+# restart apache2
+systemctl restart apache2
+systemctl restart mysql
 
 # Final message
-
 echo "#########################################################################################################
       #                               Your webserver is now installed                             #
       #                                                                                           #
@@ -127,19 +140,10 @@ echo "##########################################################################
       #  php7.4-common php7.4-mysql php7.4-curl php7.4-json php7.4-zip php7.4-cgi php7.4-opcache  #
       #              php7.4-mbstring php7.4-xml php7.4-bcmath php7.4-mysqli php7.4-gd             #
       #                                                                                           #                               
-      #                                     Domain Info:                                          #
-      #                     echo "Your server domain is " $ServerName                             #  
-      #                     echo "Your server alias is "  $ServerAlias                            #
-      #                                                                                           #  
-      #                                     MySQL Info:                                           #     
-      #                     echo "Your database name is " $ServerName                             #      
-      #                     echo "Your username is "     $ServerAlias                             #
-      #                     echo "Your password is "     $ServerAlias                             #  
-      #                     echo "Your host is "         $ServerAlias                             #  
-      #       YOU MUST MAKE A NOT OF THIS INFORMATION JUST NOW ITS NOT SAVED ON EXIT              #  
       #                                                                                           #
+      #                  YOUR LOGIN INFORMATION IN IS SAVED AT /var/www/info.txt                  #  
+      #                            REMOVE THIS FILE AFTER DOWNLOAD                                #
       #############################################################################################
-"
-                       
+"         
 # Exit the script
 exit 0
